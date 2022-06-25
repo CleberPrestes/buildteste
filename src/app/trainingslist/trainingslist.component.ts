e19ca9f81417e1b612125d67159fdc2c760b1944
Cleber Prestes <cleberprestes@hotmail.com>
2022-06-25 11:08:56 -0300

    teste de aguarde

--- src/app/trainingslist/trainingslist.component.ts
+++ src/app/trainingslist/trainingslist.component.ts
@@ -15,40 +15,58 @@ export class TrainingslistComponent implements OnInit {
 
   trainingListDetails: Training[] = []
 
-  trainingSaveName: string ='';
-  @Output() informacaoPai =  'Treinos'
+  dataOrigin: string='JSon-Server'
+  jsonOrigin: boolean = true
+  localOrigin: boolean= false
+  showLoading: boolean = true
+  showbntback: boolean = false
 
+  @Output() informacaoPai =  'Lista de Treinos'
   @Output() listaOutput: Training[]=[]
 
   constructor(private trainingService: TrainingserviceService) {
     this.getTrainings()
   }
+
   ngOnInit(): void {
 
     //Lista com promisses
-
-    /*
     this.trainingService
     .getTraininigWithPromise()
     .then((trainingListSave)=>(this.trainingListSave = trainingListSave))
+    .then((sholoading)=>(this.showLoading = false))
+    .then((showbntback)=>(this.showbntback = true))
     .catch((e) => {
       //erro ao pegar do json-server
       this.trainingListSave = JSON.parse(localStorage.getItem('listaTreino')!) as Training[];
-      alert('Json-sever fora de funcionamento dados apresentados são do LocalStorage')
+      this.dataOrigin = 'Local Storage'
+      this.showLoading = false;
+      this.jsonOrigin= false
+      this.localOrigin= true
+      this.showbntback = true;
+
     });
-*/
+
     //Lista com Observable
+/*
       this.trainingService
-      .getTraininigWithObservable().subscribe()
-    alert('Dados do Json-sever com Observable')
+      .getTraininigWithObservable().subscribe((data: Training[]) => {
+        this.trainingListSave = data;
+      },
+      error =>{
+        alert('Erro ao obter dados do Servidor utilizando dados Locais')
+        this.trainingListSave = JSON.parse(localStorage.getItem('listaTreino')!) as Training[];
+      this.dataOrigin = 'Local Storage'
 
+      this.jsonOrigin= false
+      this.localOrigin= true
 
+      })
 
+*/
   }
 
-
   removeTraining(item: Training){
-
     this.trainingListSave.splice(this.trainingListSave.indexOf(item) , 1)
     let jsonKeyList = JSON.stringify(this.trainingListSave)
     localStorage['listaTreino'] =jsonKeyList;
@@ -59,14 +77,11 @@ export class TrainingslistComponent implements OnInit {
     this.trainingService.removeTraining(training.id).subscribe()
   }
 
-
   //teste de get
   getTrainingDetail(training: Training){
     this.trainingListDetails = this.trainingListSave.filter((a)=>(training.name==a.name))
-
     var teste = this.trainingListSave.find((treino)=>{return treino.name ==training.name})
     const testeNome = teste?.name
-
     if(testeNome!==undefined){
       this.informacaoPai = testeNome?.toString()
     }
